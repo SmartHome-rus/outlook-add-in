28d48f4beefc222e77ecacd9758411c9b48d63b5
SmartHome-rus <a.zalburg@gmail.com>
2025-09-14 09:34:23 +0200

    deleted

--- assets/create-icons.sh
+++
@@ -1,9 +0,0 @@
-# Create placeholder icon files for GitHub Pages
-# These are basic SVG icons that will work for the add-in
-
-# You can replace these with proper PNG icons later
-echo '<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><rect width="16" height="16" fill="#0078d4"/><path d="M8 3L13 8L8 13L3 8Z" fill="white"/></svg>' > icon-16.svg
-
-echo '<svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><rect width="32" height="32" fill="#0078d4"/><path d="M16 6L26 16L16 26L6 16Z" fill="white"/></svg>' > icon-32.svg
-
-echo '<svg width="80" height="80" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg"><rect width="80" height="80" fill="#0078d4"/><path d="M40 15L65 40L40 65L15 40Z" fill="white"/></svg>' > icon-80.svg
